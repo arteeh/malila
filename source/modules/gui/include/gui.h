f08327464c577a0c76ad145fdaf7f36fb28a0001
Maarten de Jong <accounts@arteeh.com>
2021-04-27 23:25:56 +0200

    Everything is broken, i don't know what to do

--- source/modules/gui/include/gui.h
+++ source/modules/gui/include/gui.h
@@ -1,12 +1,14 @@
 #ifndef GUI_H
 #define GUI_H
 
+#define LV_HOR_RES_MAX 240
+#define LV_VER_RES_MAX 240
+
 #include <display.h>
 #include <lvgl/lvgl.h>
 #include <lvgl_riot.h>
 
-//#define LV_HOR_RES_MAX 240
-//#define LV_VER_RES_MAX 240
+
 
 void guiInit(void);
 
