43d5b4a420769b7b9124a3b66933e4d1c240015b
Maarten de Jong <accounts@arteeh.com>
2021-11-12 16:28:51 +0100

    Delete non-lvgl stuff

--- source/main.c
+++ source/main.c
@@ -11,65 +11,12 @@ int main(void)
 	gpio_set(BUTTON0_ENABLE);
 	gpio_set(VIBRATOR);
 #endif
-	
-	displayinit();
-	displaybrightness(100);
-	
-	graphicsinit();
-	
-	thread_create(
-		threaddisplayupdatestack,
-		sizeof(threaddisplayupdatestack),
-		THREAD_PRIORITY_MAIN - 1,
-		THREAD_CREATE_STACKTEST,
-		threaddisplayupdate,
-		NULL,
-		NULL
-	);
-	
+
 	while(true)
 	{
-		graphicsclear();
-		
-		uint8_t x;
-		uint8_t y;
-		char c;
-		
-		x = 0;
-		y = 0;
-		c = ' ';
-		
-		while(c < '~')
-		{
-			x = graphicscharacter(x,y,c,small,3);
-			if(x == 0) y += 24;
-			c++;
-		}
-		
-		x = 0;
-		y += 24;
-		c = '0';
-		
-		while(c < ':')
-		{
-			x = graphicscharacter(x,y,c,big,3);
-			//if(x == 0) y += 64;
-			c++;
-		}
-		
 		xtimer_msleep(3000);
 	}
 	
 	return 0;
 }
 
-void *threaddisplayupdate(void *arg)
-{
-	(void) arg;
-
-	while(true)
-	{
-		displayupdate();
-		xtimer_msleep(3000);
-	}
-}
--- source/main.h
+++ source/main.h
@@ -1,10 +1,3 @@
 #include <xtimer.h>
 #include <board.h>
 #include <cst816s.h>
-
-#include "display.h"
-#include "graphics.h"
-
-char threaddisplayupdatestack[THREAD_STACKSIZE_MAIN];
-
-void *threaddisplayupdate(void *arg);
